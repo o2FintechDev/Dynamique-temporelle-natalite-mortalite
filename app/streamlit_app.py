6d195d700a3d4c1239039ce35b9081ec64520768
Aude <audebernier19@gmail.com>
2026-02-13 11:36:34 +0100

    env pb to stremlit

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,8 +6,7 @@ import json
 import requests
 import re
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv() 
+
 REPO_ROOT = Path(__file__).resolve().parents[1]
 if str(REPO_ROOT) not in sys.path:
     sys.path.insert(0, str(REPO_ROOT))
